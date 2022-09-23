bd74cb0734a6239a455379bae1b7af720136bd4d
Neelam <deshmane.neelam@gmail.com>
2022-09-23 14:31:26 -0700

    Neelam:Resolved merge

--- src/test/java/com/sdet/lms/utilities/BaseClass.java
+++ src/test/java/com/sdet/lms/utilities/BaseClass.java
@@ -5,8 +5,8 @@ import java.time.Duration;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.testng.annotations.AfterClass;
-import org.testng.annotations.BeforeClass;
 
+import org.testng.annotations.BeforeClass;
 import io.github.bonigarcia.wdm.WebDriverManager;
 
 public class BaseClass {
