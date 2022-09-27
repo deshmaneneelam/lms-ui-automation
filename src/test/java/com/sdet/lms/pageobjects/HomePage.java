1e4b5fb6fd98de4c2f88456a95d7dca71d8d4035
Kanupriya <k.kanupriya@gmail.com>
2022-09-27 12:41:30 -0500

    util file manually saved

--- src/test/java/com/sdet/lms/pageobjects/HomePage.java
+++ src/test/java/com/sdet/lms/pageobjects/HomePage.java
@@ -1,7 +1,102 @@
 package com.sdet.lms.pageobjects;
 
-import com.sdet.lms.utilities.BaseClass;
+import org.openqa.selenium.JavascriptExecutor;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+import org.openqa.selenium.support.PageFactory;
 
-public class HomePage extends BaseClass {
+public class HomePage {
 
+	WebDriver driver;
+	JavascriptExecutor js;
+
+	public HomePage(WebDriver driver) {
+		this.driver = driver;
+		js = (JavascriptExecutor) driver;
+		PageFactory.initElements(driver, this);
+	}
+
+	@FindBy(xpath = "//span[contains(text(),'Program')]")
+	WebElement ProgramBtn;
+
+	@FindBy(xpath = "//span[contains(text(),' LMS - Learning Management System ')]")
+	WebElement pageHeading;
+
+	@FindBy(xpath = "//span[text()='Batch']")
+	WebElement batchBtn;
+
+	@FindBy(xpath = "//span[text()='User']")
+	WebElement userBtn;
+
+	@FindBy(xpath = "//span[text()='Assignment']")
+	WebElement assignmentBtn;
+
+	@FindBy(xpath = "//span[text()='Attendance']")
+	WebElement attendanceBtn;
+
+	@FindBy(xpath = "//span[text()='Logout']")
+	WebElement logoutBtn;
+
+	public String verifyhomePageTitle() {
+		return driver.getTitle();
+	}
+
+	public String verifyhomepageHeading() {
+		return pageHeading.getText();
+	}
+
+	public boolean verifyprogramBtn() {
+		return ProgramBtn.isDisplayed();
+	}
+
+	public boolean verifybatchBtn() {
+		return batchBtn.isDisplayed();
+	}
+
+	public boolean verifyuserBtn() {
+		return userBtn.isDisplayed();
+	}
+
+	public boolean verifyassignmentBtn() {
+		return assignmentBtn.isDisplayed();
+	}
+
+	public boolean verifyattendanceBtn() {
+		return attendanceBtn.isDisplayed();
+	}
+
+	public boolean verifylogoutBtn() {
+		return logoutBtn.isDisplayed();
+	}
+
+	public String verifyprogramBtnText() {
+		System.out.println("ProgramBtn text is: " + ProgramBtn.getText());
+		return ProgramBtn.getText();
+	}
+
+	public String verifybatchBtnText() {
+		System.out.println("BatchBtn text is: " + batchBtn.getText());
+		return batchBtn.getText();
+	}
+
+	public String verifyuserBtnText() {
+		System.out.println("UserBtn text is: " + userBtn.getText());
+		return userBtn.getText();
+	}
+
+	public String verifyassignmentBtnText() {
+		System.out.println("AssignmentBtn text is: " + assignmentBtn.getText());
+		return assignmentBtn.getText();
+	}
+
+	public String verifyattendanceBtnText() {
+		System.out.println("AttendanceBtn text is: " + attendanceBtn.getText());
+		return attendanceBtn.getText();
+	}
+
+	public String verifylogoutBtnText() {
+		System.out.println("LogoutBtn text is: " + logoutBtn.getText());
+		return logoutBtn.getText();
+	}
 }
--- src/test/java/com/sdet/lms/utilities/Util.java
+++ src/test/java/com/sdet/lms/utilities/Util.java
@@ -16,48 +16,53 @@ public class Util {
 	public static final String SEARCH_TEXT = "Search...";
 	public static final String IN_TOTAL_STR1 = "In total there are ";
 	public static final String IN_TOTAL_STR2 = " programs.";
-	public static final String IN_TOTAL_STR3 = " programs.";
+
+	public static final String IN_TOTAL_STR3 = "assignments.";
+
 	public static final String CHECK_HIGHLIGHT = "p-highlight";
 	public static final String CONFIRM_ALERT = "Confirm";
 	public static final String DELETE_SUCCESS_MSG = "Successful Program Deleted";
 	public static final String LOGIN_HEADING = "Please login to LMS application";
 	public static final String SUCCESS_STR = "Successful";
 	public static final String DELETE_MULTIPLE_MSG = "Successful Programs Deleted";
-	
 
 	public static boolean isAlphaNumeric(String s) {
-        return s != null && s.matches("^[a-zA-Z0-9]*$");
-    }
+		return s != null && s.matches("^[a-zA-Z0-9]*$");
+	}
 
-	  
-		public static Map <String,String> getScenarioData() {
-			 Map <String,String> scenarioDataMap=new HashMap<>();
-			 List<String> scenarioDataList;
-			 try {
-			    
-			   String fileName="src/test/resources/testdata/LMSRegistrationData.csv";
-			    try (Stream<String> stream = Files.lines(Paths.get(fileName))) {
-			    	scenarioDataList = stream.collect(Collectors.toList());
-			    	 
-			}
-				
-			    for (String scenario:scenarioDataList) {
-			    	 
-					String data[]=scenario.split("\\,");
-			    	if (data.length ==2 )
-			            scenarioDataMap.put(data[0],data[1]);
-			    	else if (data.length==1)
-			    		scenarioDataMap.put(data[0],null);
-			    }
-				} catch (Exception e) {
-					// TODO Auto-generated catch block
-					e.printStackTrace();
-				}
-			 return scenarioDataMap;
-			
+	public static Map<String, String> getScenarioData() {
+		Map<String, String> scenarioDataMap = new HashMap<>();
+		List<String> scenarioDataList;
+		try {
+
+			String fileName = "src/test/resources/testdata/LMSRegistrationData.csv";
+			try (Stream<String> stream = Files.lines(Paths.get(fileName))) {
+				scenarioDataList = stream.collect(Collectors.toList());
+
 			}
 
-	
-	
+			for (String scenario : scenarioDataList) {
+
+				String data[] = scenario.split("\\,");
+				if (data.length == 2)
+					scenarioDataMap.put(data[0], data[1]);
+				else if (data.length == 1)
+					scenarioDataMap.put(data[0], null);
+			}
+		} catch (Exception e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		return scenarioDataMap;
+
+	}
+
+	public static final String HOME_PAGE_HEADING = "LMS - Learning Management System";
+	public static final String PROGRAM_BTN = "Program";
+	public static final String BATCH_BTN = "Batch";
+	public static final String USER_BTN = "User";
+	public static final String ASSIGNMENT_BTN = "Assignment";
+	public static final String ATTENDANCE_BTN = "Attendance";
+	public static final String LOGOUT_BTN = "Logout";
 
 }
