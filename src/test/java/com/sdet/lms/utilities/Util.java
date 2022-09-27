3eb390d850593f36fecf1440f5660184a60273c0
Grishmamali <you@example.com>
2022-09-27 00:35:59 -0500

    missed variable in util

--- src/test/java/com/sdet/lms/utilities/Util.java
+++ src/test/java/com/sdet/lms/utilities/Util.java
@@ -16,6 +16,7 @@ public class Util {
 	public static final String SEARCH_TEXT = "Search...";
 	public static final String IN_TOTAL_STR1 = "In total there are ";
 	public static final String IN_TOTAL_STR2 = " programs.";
+	public static final String IN_TOTAL_STR3 = " programs.";
 	public static final String CHECK_HIGHLIGHT = "p-highlight";
 	public static final String CONFIRM_ALERT = "Confirm";
 	public static final String DELETE_SUCCESS_MSG = "Successful Program Deleted";
